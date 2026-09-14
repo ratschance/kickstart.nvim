6a42e6dcba391a5d926787fc9789cf7f81766a30
C R <ratschance@gmail.com>
2026-09-13 23:04:18 -0500

    plugins: add markdown

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    -- Pure Lua in-buffer markdown enhancement (renders headers, tables, callouts, and bullets)
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
+    ft = { 'markdown', 'markdown.mdx' },
+    opts = {
+      heading = {
+        sign = false,
+        icons = { '󰲡 ', '󰲣 ', '󰲥 ', '󰲧 ', '󰲩 ', '󰲫 ' },
+      },
+    },
+  },
+}
